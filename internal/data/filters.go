efdea98099bec2f2028d7188f096cc6918755d6a
dmytromaimesko <dima.maimesko@gmail.com>
2026-08-14 01:28:56 +0300

    Paginating lists

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -14,9 +14,6 @@ type Filters struct {
 	SortSafelist []string
 }
 
-// Check that the client-provided Sort field matches one of the entries in our safelist
-// and if it does, extract the column name from the Sort field by stripping the leading
-// hyphen character (if one exists).
 func (f Filters) sortColumn() string {
 	if slices.Contains(f.SortSafelist, f.Sort) {
 		return strings.TrimPrefix(f.Sort, "-")
@@ -25,14 +22,20 @@ func (f Filters) sortColumn() string {
 	panic("unsafe sort parameter: " + f.Sort)
 }
 
-// Return the sort direction ("ASC" or "DESC") depending on the prefix character of the
-// Sort field.
 func (f Filters) sortDirection() string {
 	if strings.HasPrefix(f.Sort, "-") {
 		return "DESC"
 	}
 
 	return "ASC"
+}
+
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
 }
 
 func ValidateFilters(v *validator.Validator, f Filters) {
